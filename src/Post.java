e7b2ab69dad2cbbe8a2c9f53b5f188a9c66a1315
Dimage Sapelkin <kerabromsmu@users.noreply.github.com>
2025-09-24 14:25:54 +0200

    * postMessage signature changed

--- src/Post.java
+++ src/Post.java
@@ -2,7 +2,14 @@ import java.util.ArrayList;
 
 public class Post implements Likeable {
     ArrayList<User> likes = new ArrayList<>();
+    String content;
+    String destination;
     public ArrayList<User> getLikes() {
         return likes;
+    }
+
+    Post(String content, String destination) {
+        this.content = content;
+        this.destination = destination;
     }
 }
