0d27f07efd80780638eedd08f0fafbdf46687c02
Dimage Sapelkin <kerabromsmu@users.noreply.github.com>
2025-09-24 14:45:22 +0200

    * main menu implemented

--- src/SocialNetwork.java
+++ src/SocialNetwork.java
@@ -1,12 +1,14 @@
 import Helpers.Menu;
-import Helpers.MenuHelper;
 
 import java.util.ArrayList;
 
+import static Helpers.MenuHelper.*;
+
 public class SocialNetwork implements Menu {
     private static SocialNetwork instance;
-    private ArrayList<Post> posts;
-    private ArrayList<User> users;
+
+    public final ArrayList<Post> posts = new ArrayList<>();
+    public final ArrayList<User> users = new ArrayList<>();
     private SocialNetwork() {
 
     }
@@ -19,7 +21,7 @@ public class SocialNetwork implements Menu {
 
     @Override
     public void menu() {
-        MenuHelper.menuLoop("Social Network\n\nNext action:",
+        menuLoop("Social Network\n\nNext action:",
                 new String[] {"Exit", "See feed", "List users", "Log in as user"},
                 new Runnable[] {this::seeFeed, this::listUsers, this::logIn},
                 false);
@@ -27,14 +29,26 @@ public class SocialNetwork implements Menu {
     }
 
     private void seeFeed() {
-
+        if (posts.isEmpty()) {
+            System.out.println("No posts found.");
+            return;
+        }
+        for (Post post : posts) {
+            System.out.println(post);
+        }
     }
 
     private void listUsers() {
-
+        if (users.isEmpty()) {
+            System.out.println("There are no users.");
+            return;
+        }
+        for (User user : users) {
+            System.out.println("* " + user.getName() + " - " + user.getClass().getSimpleName());
+        }
     }
 
     private void logIn() {
-
+        listMenuLoop("Select user", "Cancel", "No users found.", users, User::menu, true);
     }
 }
