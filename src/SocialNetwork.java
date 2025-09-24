9da1b0149c64f3045318637788836316b40f01ad
Dimage Sapelkin <kerabromsmu@users.noreply.github.com>
2025-09-24 14:22:20 +0200

    * static SocialNetwork instance + call menu from main()

--- src/SocialNetwork.java
+++ src/SocialNetwork.java
@@ -3,13 +3,13 @@ import Helpers.Menu;
 import java.util.ArrayList;
 
 public class SocialNetwork implements Menu {
-    private SocialNetwork instance;
+    private static SocialNetwork instance;
     private ArrayList<Post> posts;
     private ArrayList<User> users;
     private SocialNetwork() {
 
     }
-    public SocialNetwork getInstance() {
+    public static SocialNetwork getInstance() {
         if (instance == null) {
             instance = new SocialNetwork();
         }
