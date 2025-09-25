887bba26dfff822cbc1ab7b6e279529854ef784d
unknown <sara.kluge@hotmail.com>
2025-09-25 09:20:04 +0200

    add AdminUser and RegularUser

--- src/AdminUser.java
+++ src/AdminUser.java
@@ -6,20 +6,39 @@ public class AdminUser extends User {
 
     @Override
     public Post postMessage(String message, String destination) {
-        Post newPost = new Post();
+        Post newPost = new Post(message, destination);
+        SocialNetwork.getInstance().posts.add(newPost);
 
-        //need addPost method in SocialNetwork
-       // SocialNetwork.getInstance().addPost(newPost);
         System.out.println("Admin " + name + " posted to " + destination + ": " + message);
 
         return newPost;
     }
 
     public void deletePost(Post post) {
-        //need removePost in SocialNetwork
-        //SocialNetwork.getInstance().removePost(post);
-
+        SocialNetwork.getInstance().posts.remove(post);
         System.out.println("Admin" + name + " deleted post: " + post);
+    }
+
+    @Override
+    public void menu() {
+        String[] options = {"Exit", "Post a message", "Delete post"};
+        Runnable[] actions = {
+                () -> postMessage("Hello from AdminUser menu!", "My Wall"),
+                () -> {
+                    Helpers.MenuHelper.listMenuLoop(
+                            "Select a post to delete:",
+                            "Cancel",
+                            "No posts available",
+                            SocialNetwork.getInstance().getPosts(),
+                            (Post post) -> {
+                                deletePost(post);
+                            },
+                            true
+                    );
+                }
+        };
+        String header = name + "'s Menu";
+        Helpers.MenuHelper.menuLoop(header, options, actions, false);
     }
 
 }
